b339729a1e7d2ab090e3d773d94157c8dbd862a1
nabeatsu <18596776+tanabe1478@users.noreply.github.com>
2020-11-30 19:07:22 +0900

    [modify] Descriptionの変更

--- Sources/Blog/main.swift
+++ Sources/Blog/main.swift
@@ -17,7 +17,7 @@ struct Blog: Website {
     // Update these properties to configure your website:
     var url = URL(string: "https://tanabe1478.com")!
     var name = "t__nabe1478's Blog"
-    var description = "t__nabe1478のブログです。務め先のブログには書く程でない技術に関することや私事について書きます。"
+    var description = "t__nabe1478のブログです。務め先のブログには書く程でないプログラミングに関することや私事について書きます。"
     var language: Language { .english }
     var imagePath: Path? { "Resources/images/logo.png" }
 }
