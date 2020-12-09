659f2038db6fb7914a72b78c7fd6e57a61dbf33a
nabeatsu <18596776+tanabe1478@users.noreply.github.com>
2020-12-09 22:42:41 +0900

    [modify] Refactor

---
+++ Sources/Blog/Theme/Theme+ItemDetail.swift
@@ -0,0 +1,35 @@
+//
+//  File.swift
+//  
+//
+//  Created by tanabe.nobuyuki on 2020/12/09.
+//
+
+import Foundation
+import Publish
+import Plot
+
+
+extension MyHtmlFactory {
+    func makeItemHTML(for item: Item<Site>, context: PublishingContext<Site>) throws -> HTML {
+        HTML(
+            .lang(context.site.language),
+            .head(for: item, on: context.site),
+            .body(
+                .class("item-page"),
+                .header(for: context, selectedSection: item.sectionID),
+                .wrapper(
+                    .article(
+                        .div(
+                            .class("content"),
+                            .contentBody(item.body)
+                        ),
+                        .span("Tagged with: "),
+                        .tagList(for: item, on: context.site)
+                    )
+                ),
+                .footer(for: context.site)
+            )
+        )
+    }
+}
